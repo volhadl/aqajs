fc633e311e8de4f1735231daf3a5ec1ec816b146
Volha Dluhakanskaya <ms.dlugokanskaya@gmail.com>
2020-02-04 10:42:42 +0300

    code review 1 rename describe and it blocks 1 Roaming spell fix 3 rename navigateByUrl  to openUrl 4 rename typeText to performSearch 4 rename const text to iPhone11 5 remane folders and files using camelCase 6 remame data in imports 7 remane search var to searchInput 8 add virification of full URL

--- testData/homeData.js
+++ testData/homeData.js
@@ -1,3 +1,3 @@
 export const homeUrl = 'https://www.mts.by';
-export const text = 'iPhone11 2019'
+export const iPhone11 = 'iPhone11 2019'
 export const title = 'Поиск по сайту'
--- test_data/roaming_data.js
+++
@@ -1,4 +0,0 @@
-export const roamingUrl = 'https://www.mts.by/services/roaming/world-travel/'
-export const tariffFirstName = '1 ГБ за границей'
-export const tariffSecondName = 'Безлимитный интернет в роуминге'
-export const tariffThirdName = 'Открытый интернет в роуминге'
