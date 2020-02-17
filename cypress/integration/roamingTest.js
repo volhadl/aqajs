2c9238e77e179b232ad73828392fd159f55baf23
Volha <ms.dlugokanskaya@gmail.com>
2020-02-17 06:13:29 +0300

    add search test add methods selectBrand, clickAdvancedSearch, selectMaterial, fillInRam, selectYear, clickShow add locators in shop data
    add openInternet test,
    associated locators
    
    remove useless locator brandList
    remove commented lines
    selectBrand, selectMaterial, fillInRam, selectYear add parameters and returns

--- cypress/integration/roamingTest.js
+++ cypress/integration/roamingTest.js
@@ -1,17 +1,29 @@
-import RoamingPage from '../../pages/roamingPage'
-import HomePage from '../../pages/homePage'
+import RoamingPage from '../../src/pages/roamingPage'
+import OpenInternetPage from '../../src/pages/openInternetPage'
+import HomePage from '../../src/pages/homePage'
 import * as roamingData from '../../testData/roamingData'
+import * as openInternetData from '../../testData/openInternetData'
 
 describe("Titles verification for roaming tariffs", ()=>{
     
     // https://app.qase.io/case/CYAQA-47
     // https://app.qase.io/case/CYAQA-48
-    it("Verify titles of roaming tariffs", ()=>{
-        HomePage.openUrl(roamingData.roamingUrl);
+    before(function() {
+        HomePage.openUrl(roamingData.roamingUrl);      })
+
+    it("Verify titles of roaming tariffs", ()=>{        
         cy.url().should('eq', roamingData.roamingUrl)
-        
+
         cy.get(RoamingPage.tariffOne).should('have.text', roamingData.tariffNameOne)
         cy.get(RoamingPage.tariffTwo).should('have.text', roamingData.tariffNameTwo)
         cy.get(RoamingPage.tariffThree).should('have.text', roamingData.tariffNameThree)
+    })
+
+    it("Verify availability of otkrytyy-internet-v-rouminge", ()=>{
+        cy.get(RoamingPage.tariffThreeShowButton).click()
+        cy.url().should('eq', openInternetData.urlThreeTariff)
+        cy.get(OpenInternetPage.title).should('have.text', roamingData.tariffNameThree)
+        //cy.get(OpenInternetPage.priceFrom).contains(openInternetData.priceFrom)
+        //cy.get(OpenInternetPage.priceTo).contains(openInternetData.priceTo)
     })
 })
---
+++ src/pages/openInternetPage.js
@@ -0,0 +1,6 @@
+class OpenInternetPage {
+    title = '.info-item__content--centered > h2'
+    priceFrom = '.tariff-sum:nth-child(3) > div'
+    priceTo = '.tariff-sum:nth-child(4) > div'
+}
+export default new OpenInternetPage();
