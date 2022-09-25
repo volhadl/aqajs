e1000f59ce82bfb9243646f924a76bbf17d6d327
Volha Dl <ms.dlugokanskaya@gmail.com>
2022-09-25 10:10:34 -0500

    refactoring of screen prot page

--- cypress/integration/screenProtectorTest.js
+++ cypress/integration/screenProtectorTest.js
@@ -1,42 +1,45 @@
+/// <reference types = "cypress"/>
+
 import HomePage from '../../src/pages/HomePage'
 import ScreenPage from '../../src/pages/ScreenProtectorPage'
-import ShopPhonePage from '../../src/pages/ShopPhonePage'
 import AccessoriesPage from '../../src/pages/AccessoriesPage'
 import * as screenData from '../../testData/screenProtectorData'
 import * as shopPhoneData from '../../testData/shopPhoneData'
 import * as urls from '../../testData/urls'
+import ScreenProtectorPage from '../../src/pages/ScreenProtectorPage'
 
 describe("Verification of filters for screen protector", ()=>{
     let priceTwoValue = 0
-
+    
     beforeEach(function() {
         HomePage.openUrl(urls.accessoriesUrl)  
-        ScreenPage.selectType(AccessoriesPage.screenProtectionGlass)
+        ScreenPage.selectType(AccessoriesPage.screenProtectionType)
+        cy.contains(screenData.found).should('be.visible')
     })
 
-    it("Verify redirect to screen protector page", ()=>{      
-        HomePage.openUrl(urls.accessoriesUrl)  
-        AccessoriesPage.selectAccessory(AccessoriesPage.protection)
-        AccessoriesPage.selectType(AccessoriesPage.screenType)
+    it("Verify screen protector page selection", ()=>{      
         cy.get(ScreenPage.firstItem).should('contain', screenData.sceenTitle)
     })
 
     it("Verification of sorting by color if 'Черный' is selected", ()=>{  
         ScreenPage.selectColor(ScreenPage.blackColor)
-        cy.get(ScreenPage.firstItem).should('contain', screenData.blackFrame)
+        cy.get(ScreenProtectorPage.allProducts).should('contain', screenData.blackFrame)
     })
 
-    it("Verification of sorting by 'Модель вашего устройства' if 'iPhone 12 Pro Max Model' is selected", ()=>{
+    xit("Verification of sorting by 'Модель вашего устройства' if 'iPhone 12 Pro Max Model' is selected", ()=>{
+        // today there is no way to select a model
         ScreenPage.selectModel(ScreenPage.iPhone12ProMaxModel)
         cy.get(ScreenPage.firstItem).should('contain', screenData.iPhoneModel)
     })
 
-    it("Verification of sorting by 'Производитель вашего устройства' if 'Apple' is selected", ()=>{
+    xit("Verification of sorting by 'Производитель вашего устройства' if 'Apple' is selected", ()=>{
+        // today there is no way to select a model
         ScreenPage.selectBrand(ScreenPage.appleBrand)
         cy.get(ScreenPage.firstItem).should('contain', shopPhoneData.iPhoneBrand)
     })
 
     xit("Verification of sorting by 'Цена'", ()=>{
+        // there is a bug on ui. filtering by price returns nothing or not screens
         ScreenPage.fillInPrice(screenData.priceFrom, screenData.priceTo)
         cy.get(ScreenPage.priceTwo).then(($strong) => {
             priceTwoValue = parseFloat($strong.text())
--- testData/screenProtectorData.js
+++ testData/screenProtectorData.js
@@ -2,4 +2,5 @@ export const sceenTitle = "Стекло защитное"
 export const blackFrame = "черная рамка"
 export const iPhoneModel = "iPhone"
 export const priceFrom = 6
-export const priceTo = 26
+export const priceTo = 36
+export const found = 'Найдено'
