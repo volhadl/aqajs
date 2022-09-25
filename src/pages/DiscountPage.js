7c2f9a7e9175cbc96c03540b24f04e113cc4ca94
Volha Dl <ms.dlugokanskaya@gmail.com>
2022-09-25 10:29:51 -0500

    refactoring of the discount test

--- src/pages/DiscountPage.js
+++ src/pages/DiscountPage.js
@@ -1,6 +1,6 @@
 class DiscountPage {
-    oneColumn = "tr:nth-child(n+3) > td:nth-child(2) > p"
-    twoColumn = "tr:nth-child(n+3) > td:nth-child(7) > p"
-    threeColumn = "tr:nth-child(n+3) > td:nth-child(8) > p"
+    oneColumn = "tr:nth-child(n+1) > td:nth-child(2) > p"
+    twoColumn = "tr:nth-child(n+1) > td:nth-child(9) > p"
+    threeColumn = "tr:nth-child(n+1) > td:nth-child(10) > p"
 }
 export default new DiscountPage();
